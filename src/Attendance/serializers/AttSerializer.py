a8b5118addeee15f97d10c95b6cb35196385932b
aimthecoder <ahmedsom3a40@hotmail.com>
2022-06-12 22:56:59 +0200

    validate check in and out

--- src/Attendance/serializers/AttSerializer.py
+++ src/Attendance/serializers/AttSerializer.py
@@ -1,15 +1,16 @@
 from Attendance.models import Attendance
 from rest_framework import serializers
-from datetime import date
+from datetime import datetime
 
 class AttendanceSerializer(serializers.ModelSerializer):
     class Meta:
         model = Attendance
         fields = '__all__'
     def validate(self, data):
-        today_att = Attendance.objects.all().filter(date=date.today)
-        if today_att:
-            if data['check'] == 'out' and today_att.check != 'in':
-                raise serializers.ValidationError("you must check in first")
+        today_att = Attendance.objects.all().filter(date=datetime.now(), check='in')
+        if not today_att and  data['check'] == 'out':
+            raise serializers.ValidationError("you must check in first")
+        if data['check'] == 'out' and data['check'] < today_att.date:
+            raise serializers.ValidationError("CHECK OUT MUST OCCUR AFTER CHECK IN")
         return data
 
