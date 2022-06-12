85e42cbf61cac6f661ad26588eb4819bac45b916
aimthecoder <ahmedsom3a40@hotmail.com>
2022-06-12 22:49:33 +0200

    change date field to be timestamp

--- src/Attendance/serializers/AttSerializer.py
+++ src/Attendance/serializers/AttSerializer.py
@@ -1,12 +1,15 @@
 from Attendance.models import Attendance
 from rest_framework import serializers
+from datetime import date
 
 class AttendanceSerializer(serializers.ModelSerializer):
     class Meta:
         model = Attendance
         fields = '__all__'
     def validate(self, data):
-        if data['check_in'] >= data['check_out']:
-            raise serializers.ValidationError("check_out must occur after check_in")
+        today_att = Attendance.objects.all().filter(date=date.today)
+        if today_att:
+            if data['check'] == 'out' and today_att.check != 'in':
+                raise serializers.ValidationError("you must check in first")
         return data
 
