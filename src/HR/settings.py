d3398887f6dd1574ffb6ea633bc594a0f3877512
aimthecoder <ahmedsom3a40@hotmail.com>
2022-06-12 17:01:19 +0200

    update secret key

--- src/HR/settings.py
+++ src/HR/settings.py
@@ -9,19 +9,19 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
+from env import get_env
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+env_variables = get_env('env.json', {}, os.curdir)
+SECRET_KEY = env_variables['sk']
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-e!5xkbsjkero_3f97f=5btg$mlwl3jp0%q60r4$o@vo=xhxx_s'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
