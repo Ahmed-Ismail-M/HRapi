67d99de74cd2129c776a0ce72103598dce8ae19b
aimthecoder <ahmedsom3a40@hotmail.com>
2022-06-12 17:12:42 +0200

    add employee and attendance models

--- src/Attendance/models.py
+++ src/Attendance/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+class Employee(User):
+    class Meta:
+        verbose_name = "employee"
+        verbose_name_plural = 'employees'
+class Attendance(models.Model):
+    emp = models.ForeignKey(Employee, on_delete=models.CASCADE,  related_name='employee')
+    check_in = models.TimeField()
+    check_out = models.TimeField()
+    date = models.DateField
 # Create your models here.
