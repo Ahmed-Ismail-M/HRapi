d88d904cc1a4ea3882718ddd21cfd26fdb696d5d
aimthecoder <ahmedsom3a40@hotmail.com>
2022-06-12 22:04:44 +0200

    fix: import date today

--- src/Attendance/models.py
+++ src/Attendance/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-from datetime import datetime
+from datetime import date
 class User(AbstractUser):
     pass
 class Employee(User):
@@ -11,5 +11,5 @@ class Attendance(models.Model):
     emp = models.ForeignKey(Employee, on_delete=models.CASCADE,  related_name='employee')
     check_in = models.TimeField()
     check_out = models.TimeField()
-    date = models.DateField(default=datetime.date.today)
+    date = models.DateField(default=date.today)
 # Create your models here.
