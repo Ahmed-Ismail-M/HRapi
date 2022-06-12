ddd81b19509eea57b9bc978cc7c6999502e45a89
aimthecoder <ahmedsom3a40@hotmail.com>
2022-06-12 22:04:58 +0200

    format doc

--- src/Attendance/models.py
+++ src/Attendance/models.py
@@ -1,15 +1,21 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from datetime import date
+
+
 class User(AbstractUser):
     pass
+
+
 class Employee(User):
     class Meta:
         verbose_name = "employee"
         verbose_name_plural = 'employees'
+
+
 class Attendance(models.Model):
-    emp = models.ForeignKey(Employee, on_delete=models.CASCADE,  related_name='employee')
+    emp = models.ForeignKey(
+        Employee, on_delete=models.CASCADE,  related_name='employee')
     check_in = models.TimeField()
     check_out = models.TimeField()
     date = models.DateField(default=date.today)
-# Create your models here.
