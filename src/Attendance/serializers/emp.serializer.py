bf2151443c317d7efe8cc0b2efc3a8d66bd124af
aimthecoder <ahmedsom3a40@hotmail.com>
2022-06-12 17:15:32 +0200

    add emp serializer

---
+++ src/Attendance/serializers/emp.serializer.py
@@ -0,0 +1,7 @@
+from Attendance.models import Employee
+from rest_framework import serializers
+
+class EmployeeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Employee
+        fields = "__all__"
