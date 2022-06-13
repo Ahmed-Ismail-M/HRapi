5fc82557c37134a3b0e848ce72e83f329fba2532
aimthecoder <ahmedismailm85@gmail.com>
2022-06-13 09:53:26 +0200

    format doc

--- src/Attendance/middleware.py
+++ src/Attendance/middleware.py
@@ -3,7 +3,6 @@ from django.http import HttpResponse
 
 def auth_required(view_func):
     def wrapper_func(request, *args, **kwargs):
-        print(request.user)
         if request.user.is_authenticated:
             return view_func(request, *args, **kwargs)
         else:
