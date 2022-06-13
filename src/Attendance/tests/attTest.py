15dbd79fdca946ff9cda61133f8546fb5279658b
aimthecoder <ahmedismailm85@gmail.com>
2022-06-13 09:46:07 +0200

    add error test

--- src/Attendance/tests/attTest.py
+++ src/Attendance/tests/attTest.py
@@ -21,6 +21,13 @@ class AddAtt(APITestCase):
         self.emp.delete()
 
     def test_att(self):
-        data = {"check_in": "1:30", "date": "2022-1-1", }
+        data = {"check_in": "1:30", "date": "2022-1-1", "check_out": "1:00"}
         response = self.client.post("/api/v1/attendance", data)
+        print(response.content)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_missed_checkout(self):
+        data = {"check_in": "1:30", "date": "2022-1-1"}
+        response = self.client.post("/api/v1/attendance", data)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.content, b'{"check_out":["This field is required."]}')
