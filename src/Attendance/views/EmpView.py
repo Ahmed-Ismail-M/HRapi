e01a39b303d10bd892250c662592589722d26aa5
aimthecoder <ahmedismailm85@gmail.com>
2022-06-13 07:31:35 +0200

    remove token auth

--- src/Attendance/views/EmpView.py
+++ src/Attendance/views/EmpView.py
@@ -2,7 +2,6 @@ from rest_framework import generics
 from Attendance.serializers.EmpSerializer import EmployeeRegisterSerializer, EmployeeLoginSerializer
 from django.contrib.auth.models import Group
 from django.contrib.auth import login
-from rest_framework.authtoken.models import Token
 from rest_framework import status
 from rest_framework.response import Response
 
@@ -18,9 +17,8 @@ class RegisterAPI(generics.GenericAPIView):
         group.save()
         user.groups.add(group)
         login(request, user)
-        token, created = Token.objects.get_or_create(user=serializer.instance)
         return Response(
-            {"token": token.key, "user_id": user.id}, status=status.HTTP_201_CREATED
+            {"user_id": user.id}, status=status.HTTP_201_CREATED
         )
 class LoginAPI(generics.GenericAPIView):
     serializer_class = EmployeeLoginSerializer
@@ -32,7 +30,6 @@ class LoginAPI(generics.GenericAPIView):
         serializer_class.is_valid(raise_exception=True)
         user = serializer_class.validated_data["user"]
         login(request, user)
-        token, created = Token.objects.get_or_create(user=serializer_class.instance)
         return Response(
-            {"token": token.key, "user_id": user.id}, status=status.HTTP_200_OK
+            {"user_id": user.id}, status=status.HTTP_200_OK
         )
