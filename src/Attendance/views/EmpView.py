4b8cb3c4a1f3321c9e0d468b1ec357df4d1b7f4d
aimthecoder <ahmedismailm85@gmail.com>
2022-06-13 08:49:54 +0200

    format doc

--- src/Attendance/views/EmpView.py
+++ src/Attendance/views/EmpView.py
@@ -1,6 +1,5 @@
 from rest_framework import generics
 from Attendance.serializers.EmpSerializer import EmployeeRegisterSerializer, EmployeeLoginSerializer
-from django.contrib.auth.models import Group
 from django.contrib.auth import login
 from rest_framework import status
 from rest_framework.response import Response
@@ -15,10 +14,13 @@ class RegisterAPI(generics.GenericAPIView):
         user = serializer.save()
         login(request, user)
         return Response(
-            { "user_id": user.id}, status=status.HTTP_201_CREATED
+            {"user_id": user.id}, status=status.HTTP_201_CREATED
         )
+
+
 class LoginAPI(generics.GenericAPIView):
     serializer_class = EmployeeLoginSerializer
+
     def post(self, request, *args, **kwargs):
         serializer_class = self.serializer_class(
             data=request.data, context={"request": request}
@@ -28,5 +30,5 @@ class LoginAPI(generics.GenericAPIView):
         user = serializer_class.validated_data["user"]
         login(request, user)
         return Response(
-            { "user_id": user.id}, status=status.HTTP_200_OK
+            {"user_id": user.id}, status=status.HTTP_200_OK
         )
