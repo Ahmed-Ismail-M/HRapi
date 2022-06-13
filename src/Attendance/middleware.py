1a46c42b9e1c0d3d5fc1ecb57dbdfe2f31718979
aimthecoder <ahmedsom3a40@hotmail.com>
2022-06-13 05:57:19 +0200

    format doc

--- src/Attendance/middleware.py
+++ src/Attendance/middleware.py
@@ -1,12 +1,12 @@
 from django.http import HttpResponse
 
+
 def auth_required(view_func):
     def wrapper_func(request, *args, **kwargs):
         if request.user.is_authenticated:
             return view_func(request, *args, **kwargs)
         else:
             return HttpResponse("Not Authorized", status=401)
-
     return wrapper_func
 
 
