9f68ba65e79207b9ad1a9b0b8e33c7f32bd506fe
aimthecoder <ahmedismailm85@gmail.com>
2022-06-13 09:41:16 +0200

    add required fields

--- src/Attendance/serializers/AttSerializer.py
+++ src/Attendance/serializers/AttSerializer.py
@@ -6,14 +6,19 @@ from datetime import datetime, date
 class AttendanceSerializer(serializers.ModelSerializer):
     class Meta:
         model = Attendance
-        fields = ['check_in','check_out' 'date']
+        fields = ['check_in', 'check_out' 'date']
+        extra_kwargs = {"check_in": {"required": True}, "check_out": {
+            "required": True}, "date": {"required": True}}
+
     def validate(self, data):
         today_att = Attendance.objects.get(date=date.today())
-        if today_att: # check if there is a previous record
-            if today_att.check_in > data['check_in'] or today_att.check_out < data['check_out']: # check if the new time is valid
+        if today_att:  # check if there is a previous record
+            # check if the new time is valid
+            if today_att.check_in > data['check_in'] or today_att.check_out < data['check_out']:
                 raise serializers.ValidationError("Invalid time")
         if data['check_out'] <= data['check_in']:
-            raise serializers.ValidationError("CHECK OUT MUST OCCUR AFTER CHECK IN")
+            raise serializers.ValidationError(
+                "CHECK OUT MUST OCCUR AFTER CHECK IN")
         return data
         # today_att = Attendance.objects.get(date=date.today())
         # if today_att:
@@ -23,10 +28,11 @@ class AttendanceSerializer(serializers.ModelSerializer):
         # if data['check'] == 'out' and data['check'] < today_att.date:
         #     raise serializers.ValidationError("CHECK OUT MUST OCCUR AFTER CHECK IN")
         # return data
+
     def create(self, validated_data):
         user = self.context['request'].user
         try:
-            emp = Employee.objects.get(pk = user.id)
+            emp = Employee.objects.get(pk=user.id)
             att = Attendance.objects.create(emp=emp, **validated_data)
         except Employee.DoesNotExist:
             raise serializers.ValidationError("Registered employee only")
