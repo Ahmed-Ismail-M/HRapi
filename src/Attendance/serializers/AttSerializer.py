0fbed23bc3116b4187ebe4574b2fc1f58df647be
aimthecoder <ahmedismailm85@gmail.com>
2022-06-13 10:27:40 +0200

    format doc

--- src/Attendance/serializers/AttSerializer.py
+++ src/Attendance/serializers/AttSerializer.py
@@ -13,24 +13,12 @@ class AttendanceSerializer(serializers.ModelSerializer):
     def validate(self, data):
         attendaces = Attendance.objects.all().filter(date=data['date'])
         for att in attendaces:
-            if att.check_in >= data['check_in']:  
-                raise serializers.ValidationError("last check in > check in")
-            if att.check_out <= data['check_out']:
-                raise serializers.ValidationError("last check out < check out")
             if att.check_out >= data['check_in']:
                 raise serializers.ValidationError("last check out > check in")
         if data['check_out'] <= data['check_in']:
             raise serializers.ValidationError(
                 "CHECK OUT MUST OCCUR AFTER CHECK IN")
         return data
-        # today_att = Attendance.objects.get(date=date.today())
-        # if today_att:
-        #     if today_att.check == data['check']:
-        #         print(date.today(), today_att.check)
-        #     raise serializers.ValidationError("you must check in first")
-        # if data['check'] == 'out' and data['check'] < today_att.date:
-        #     raise serializers.ValidationError("CHECK OUT MUST OCCUR AFTER CHECK IN")
-        # return data
 
     def create(self, validated_data):
         user = self.context['request'].user
