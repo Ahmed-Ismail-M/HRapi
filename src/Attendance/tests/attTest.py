f49fcc19747602384bc47f32e960ef9c6f39f51c
aimthecoder <ahmedismailm85@gmail.com>
2022-06-13 10:40:09 +0200

    test get all attendances

--- src/Attendance/tests/attTest.py
+++ src/Attendance/tests/attTest.py
@@ -38,3 +38,7 @@ class AddAtt(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertEqual(response.content,
                          b'{"check_out":["This field is required."]}')
+
+    def test_get_att(self):
+        response = self.client.get("/api/v1/attendances")
+        print(response.content)
