c116dbec18a4a41f667c3e7328fdf587469959f0
ahmed <ahmedismailm85@gmail.com>
2022-06-14 11:24:38 +0200

    add property late and leave

--- src/Attendance/models.py
+++ src/Attendance/models.py
@@ -9,9 +9,21 @@ class Employee(AbstractUser):
         verbose_name_plural = 'employees'
 
 class Attendance(models.Model):
-    WORKING_HRS = (5, 9)
+    WORKING_HRS = {'in':5, 'out':9}
     emp = models.ForeignKey(
         Employee, on_delete=models.CASCADE,  related_name='employee')
     check_in = models.TimeField(default=None, null=True)
     check_out = models.TimeField(default=None, null=True)
     date = models.DateField(default=date.today)
+
+    @property
+    def check_late(self) -> bool:
+        if self.check_in> self.WORKING_HRS['in']:
+            return True
+        return False
+
+    @property
+    def check_early_leave(self)-> bool:
+        if self.check_out< self.WORKING_HRS['out']:
+            return True
+        return False
