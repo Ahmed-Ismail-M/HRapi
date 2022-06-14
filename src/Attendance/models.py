0b60866e2cae989573458f70d76c8d00180dcdd3
ahmed <ahmedismailm85@gmail.com>
2022-06-14 14:32:52 +0200

    return dict of time

--- src/Attendance/models.py
+++ src/Attendance/models.py
@@ -32,9 +32,9 @@ class Attendance(models.Model):
         return False
 
     @classmethod
-    def calculate_wroking_time(cls, check_in: time, check_out: time) -> timedelta:
-        print(check_in, check_out)
-
-        return timedelta(hours=check_out.hour, minutes=check_out.minute) - timedelta(
-            hours=check_in.hour, minutes=check_in.minute
-        )
+    def calculate_wroking_time(cls, check_in: time, check_out: time) -> dict:
+        print(check_out.hour, check_in.hour)
+        td = timedelta(hours=check_out.hour, minutes=check_out.minute) - timedelta(
+            hours=check_in.hour, minutes=check_in.minute)
+        time = {'days': td.days, 'hrs': td.seconds // 3600, 'mins': (td.seconds //60)% 60}
+        return time
