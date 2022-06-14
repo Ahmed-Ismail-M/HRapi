e2e5aa7b6091f859f40e3f5997085015df34501f
ahmed <ahmedismailm85@gmail.com>
2022-06-14 18:34:12 +0200

    add Index to url

--- src/Attendance/urls.py
+++ src/Attendance/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from Attendance.views.EmpView import RegisterAPI, LoginAPI
-from Attendance.views.AttView import Create, IndexByUser, daily_index, daily_report
+from Attendance.views.AttView import Create, IndexByUser, daily_index, daily_report, Index
 
 urlpatterns = [path("api/v1/register", RegisterAPI.as_view(), name="register"),
                path("api/v1/login", LoginAPI.as_view(), name="login"),
@@ -8,5 +8,6 @@ urlpatterns = [path("api/v1/register", RegisterAPI.as_view(), name="register"),
                path("api/v1/attendances", IndexByUser.as_view(), name="get_atts"),
                path("api/v1/daily/attendances", daily_index, name="daily_index"),
                path("api/v1/report/attendances", daily_report, name="daily_report"),
+               path("api/v1/allattendances", Index.as_view(), name="index"),
                ]
                
--- src/Attendance/views/AttView.py
+++ src/Attendance/views/AttView.py
@@ -7,7 +7,12 @@ from rest_framework import status
 from django.utils.decorators import method_decorator
 from Attendance.middleware import auth_required, allowed_users
 from rest_framework.decorators import api_view
-from Attendance.datastore.att_data_store import get_daily_report_by_user, get_daily_index_by_user, get_all_attendances
+from Attendance.datastore.att_data_store import (
+    get_daily_report_by_user,
+    get_daily_index_by_user,
+    get_all_attendances,
+)
+
 
 def calc_working_hrs(check_in: datetime, check_out: datetime):
     return str(check_in - check_out)
@@ -46,7 +51,15 @@ def daily_index(request):
 def daily_report(request):
     return Response(get_daily_report_by_user(user_id=request.user.id))
 
-@auth_required
-@allowed_users(allowed_roles=["Admin"])
-def index(request):
-    return Response(get_all_attendances())
+
+class Index(generics.ListAPIView):
+    serializer_class = AttendanceSerializer
+
+    def get_queryset(self):
+        queryset = Attendance.objects.all()
+        return queryset
+
+    @method_decorator(auth_required)
+    @method_decorator(allowed_users(["Admin"]))
+    def get(self, request, *args, **kwargs):
+        return super().get(request, *args, **kwargs)
