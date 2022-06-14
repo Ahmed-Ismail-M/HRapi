470c968dab81c928b5915e462b09c1db2727503b
ahmed <ahmedismailm85@gmail.com>
2022-06-14 18:44:13 +0200

    add test to get all attendance

--- src/Attendance/tests/attTest.py
+++ src/Attendance/tests/attTest.py
@@ -8,14 +8,15 @@ from django.contrib.auth.models import Group
 class AddAtt(APITestCase):
     def setUp(self) -> None:
         self.emp = Employee.objects.create(
-            username="admin", email="admin@im-software.net")
-        self.emp.set_password('testpass')
-        group, created = Group.objects.get_or_create(name='Employee')
+            username="admin", email="admin@im-software.net", is_superuser=True
+        )
+        self.emp.set_password("testpass")
+        group, created = Group.objects.get_or_create(name="Employee")
         group.save()
         self.emp.groups.add(group)
         self.emp.save()
         self.client = Client()
-        self.client.login(username='admin', password='testpass')
+        self.client.login(username="admin", password="testpass")
 
     def tearDown(self):
         self.emp.delete()
@@ -37,9 +38,11 @@ class AddAtt(APITestCase):
         response = self.client.post("/api/v1/attendance", data)
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertEqual(
-            response.content, b'{"non_field_errors":["CHECK IN MUST OCCUR BEFORE CHECK OUT"]}')
+            response.content, b'{"non_field_errors":["PLEASE CHECK IN FIRST"]}'
+        )
 
     def test_get_att(self):
-        self.client.login(username='admin', password='testpass')
-        response = self.client.get("/api/v1/attendances")
-        print(response.content)
+        self.test_valid_check_out()
+        self.client.login(username="admin", password="testpass")
+        response = self.client.get("/api/v1/allattendances")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
